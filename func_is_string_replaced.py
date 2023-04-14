b0b5e5b658f057eaddb46d94daa82b52d41a01e0
klasenberry <klasenberry@gmail.com>
2023-04-14 15:43:06 -0700

    staging process

---
+++ func_is_string_replaced.py
@@ -0,0 +1,3 @@
+def is_string_replaced(str1,old_word, new_word):
+    new_string=str1.replace(old_word, new_word)
+print(is_string_replaced("hello world", "world", "hotdog"))
---
+++ per_scholas_practice.py
@@ -0,0 +1,6 @@
+def replace_email(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index=email.index("@"+old_domain)
+        new_email=
+        word="apple orange & others"
+        print(word.index("apple"))
